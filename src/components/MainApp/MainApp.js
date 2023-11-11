6454014a0dda7be32ece9e437ae6fe12ef2eb6ed
Greg <thegregoryjohnson@gmail.com>
2023-11-11 15:54:59 -0500

    [~] Tabs plus list styling.

--- src/components/MainApp/MainApp.js
+++ src/components/MainApp/MainApp.js
@@ -1,32 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import UserList from '../UserList/UserList';
 import './MainApp.css';
 import candidates from '../../data/candidates';
+
 const offices = ['President', 'Vice President', 'Treasurer', 'Secretary'];
 
 const MainApp = () => {
-  const [userVotes, setUserVotes] = useState({});
+  const [userVotes, setUserVotes] = useState(() => {
+    const storedVotes = localStorage.getItem('userVotes');
+    return storedVotes ? JSON.parse(storedVotes) : {};
+  });
+
+  const [results, setResults] = useState(() => {
+    const storedResults = localStorage.getItem('votingResults');
+    return storedResults ? JSON.parse(storedResults) : {};
+  });
+
+  useEffect(() => {
+    localStorage.setItem('userVotes', JSON.stringify(userVotes));
+  }, [userVotes]);
+
+  useEffect(() => {
+    localStorage.setItem('votingResults', JSON.stringify(results));
+  }, [results]);
 
   const handleVote = (userId, office) => {
-    setUserVotes(prevVotes => ({
+    setUserVotes((prevVotes) => ({
       ...prevVotes,
       [userId]: { ...prevVotes[userId], [office]: (prevVotes[userId]?.[office] || 0) + 1 },
     }));
+
+    setResults((prevResults) => ({
+      ...prevResults,
+      [office]: [...(prevResults[office] || []), userId],
+    }));
   };
 
-  const getUserVotesByOffice = office => {
+  const clearResults = () => {
+    localStorage.removeItem('userVotes');
+    localStorage.removeItem('votingResults');
+    setUserVotes({});
+    setResults({});
+  };
+
+  const getUserVotesByOffice = (office) => {
     return Object.keys(userVotes)
-      .filter(userId => userVotes[userId]?.[office])
-      .sort((a, b) => userVotes[a][office] - userVotes[b][office]);
+      .filter((userId) => userVotes[userId]?.[office])
+      .sort((a, b) => userVotes[b][office] - userVotes[a][office]);
   };
 
   return (
     <section className="gutter">
       <div className="container">
         <UserList onVote={handleVote} />
-        
+
         <h2>Results</h2>
-        {offices.map(office => (
+        <button onClick={clearResults}>Clear Results</button>
+        {offices.map((office) => (
           <div className="result-list" key={office}>
             <h3>{office} Results</h3>
             <table>
@@ -39,11 +69,11 @@ const MainApp = () => {
                 </tr>
               </thead>
               <tbody>
-                {getUserVotesByOffice(office).map(userId => (
+                {getUserVotesByOffice(office).map((userId) => (
                   <tr key={userId}>
                     <td>{userId}</td>
-                    <td>{candidates.find(candidate => candidate.id === parseInt(userId, 10)).name}</td>
-                    <td>{candidates.find(candidate => candidate.id === parseInt(userId, 10)).email}</td>
+                    <td>{candidates.find((candidate) => candidate.id === parseInt(userId, 10)).name}</td>
+                    <td>{candidates.find((candidate) => candidate.id === parseInt(userId, 10)).email}</td>
                     <td>{userVotes[userId]?.[office] || 0}</td>
                   </tr>
                 ))}
--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -1,23 +1,16 @@
 // src/components/UserList/UserList.js
 import React, { useState, useEffect } from 'react';
-import candidates from '../../data/candidates'; // Adjust the path based on your project structure
+import candidates from '../../data/candidates';
+import './UserList.css';
 
 const UserList = ({ onVote }) => {
   const [selectedOffice, setSelectedOffice] = useState('');
   const [officeCandidates, setOfficeCandidates] = useState([]);
+  const [activeButton, setActiveButton] = useState('');
 
   useEffect(() => {
     if (selectedOffice) {
-      // Group candidates by office
-      const candidatesByOffice = candidates.reduce((acc, candidate) => {
-        const office = candidate.office.toLowerCase();
-        acc[office] = acc[office] || [];
-        acc[office].push(candidate);
-        return acc;
-      }, {});
-
-      // Select candidates for the chosen office
-      const selectedCandidates = candidatesByOffice[selectedOffice.toLowerCase()] || [];
+      const selectedCandidates = candidates.filter(candidate => candidate.office.toLowerCase() === selectedOffice.toLowerCase());
       setOfficeCandidates(selectedCandidates);
     }
   }, [selectedOffice]);
@@ -29,41 +22,46 @@ const UserList = ({ onVote }) => {
   return (
     <div>
       <h2>User List Example</h2>
-      <label htmlFor="officeSelect">Select Office:</label>
-      <select
-        id="officeSelect"
-        value={selectedOffice}
-        onChange={e => setSelectedOffice(e.target.value)}
-      >
-        <option value="">Select an Office</option>
-        <option value="President">President</option>
-        <option value="Vice President">Vice President</option>
-        <option value="Treasurer">Treasurer</option>
-        <option value="Secretary">Secretary</option>
-      </select>
+      <div className="office-buttons">
+        {['President', 'Vice President', 'Treasurer', 'Secretary'].map(office => (
+          <button
+            key={office}
+            onClick={() => {
+              setSelectedOffice(office);
+              setActiveButton(office);
+            }}
+            className={activeButton === office ? 'active' : ''}
+          >
+            {office}
+          </button>
+        ))}
+      </div>
       {selectedOffice && officeCandidates.length > 0 && (
-        <table className="user-list__table">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {officeCandidates.map(candidate => (
-              <tr key={candidate.id}>
-                <td>{candidate.id}</td>
-                <td>{candidate.name}</td>
-                <td>{candidate.email}</td>
-                <td>
-                  <button onClick={() => handleVote(candidate.id)}>Vote</button>
-                </td>
+        <div className="user-list__container">
+          <h3 className="user-list__title">{selectedOffice} Candidates</h3>
+          <table className="user-list__table">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Actions</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {officeCandidates.map(candidate => (
+                <tr key={candidate.id}>
+                  <td>{candidate.id}</td>
+                  <td>{candidate.name}</td>
+                  <td>{candidate.email}</td>
+                  <td>
+                    <button onClick={() => handleVote(candidate.id)}>Vote</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       )}
     </div>
   );
