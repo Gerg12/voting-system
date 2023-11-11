d1ac09eb1c7262472b0c01c49d8e2cae41eb3bcb
Greg <thegregoryjohnson@gmail.com>
2023-11-11 14:26:35 -0500

    [~] Add candidate group select.

--- src/components/MainApp/MainApp.js
+++ src/components/MainApp/MainApp.js
@@ -1,28 +1,11 @@
-// src/components/MainApp/MainApp.js
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import UserList from '../UserList/UserList';
-import VotingSystem from '../VotingSystem/VotingSystem';
 import './MainApp.css';
-
+import candidates from '../../data/candidates';
 const offices = ['President', 'Vice President', 'Treasurer', 'Secretary'];
 
 const MainApp = () => {
   const [userVotes, setUserVotes] = useState({});
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users');
-        const result = await response.json();
-        setUsers(result);
-      } catch (error) {
-        console.error('Error fetching user data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
 
   const handleVote = (userId, office) => {
     setUserVotes(prevVotes => ({
@@ -37,23 +20,14 @@ const MainApp = () => {
       .sort((a, b) => userVotes[a][office] - userVotes[b][office]);
   };
 
-  const getUserData = userId => {
-    const user = users.find(user => user.id === parseInt(userId, 10));
-    return user ? { name: user.name, email: user.email } : { name: '', email: '' };
-  };
-
   return (
     <section className="gutter">
       <div className="container">
         <UserList onVote={handleVote} />
-        <div className="voting-systems">
-          {offices.map(office => (
-            <VotingSystem key={office} title={office} onVote={handleVote} />
-          ))}
-        </div>
+        
         <h2>Results</h2>
         {offices.map(office => (
-          <div key={office}>
+          <div className="result-list" key={office}>
             <h3>{office} Results</h3>
             <table>
               <thead>
@@ -65,17 +39,14 @@ const MainApp = () => {
                 </tr>
               </thead>
               <tbody>
-                {getUserVotesByOffice(office).map(userId => {
-                  const userData = getUserData(userId);
-                  return (
-                    <tr key={userId}>
-                      <td>{userId}</td>
-                      <td>{userData.name}</td>
-                      <td>{userData.email}</td>
-                      <td>{userVotes[userId]?.[office] || 0}</td>
-                    </tr>
-                  );
-                })}
+                {getUserVotesByOffice(office).map(userId => (
+                  <tr key={userId}>
+                    <td>{userId}</td>
+                    <td>{candidates.find(candidate => candidate.id === parseInt(userId, 10)).name}</td>
+                    <td>{candidates.find(candidate => candidate.id === parseInt(userId, 10)).email}</td>
+                    <td>{userVotes[userId]?.[office] || 0}</td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -1,26 +1,28 @@
 // src/components/UserList/UserList.js
 import React, { useState, useEffect } from 'react';
+import candidates from '../../data/candidates'; // Adjust the path based on your project structure
 
 const UserList = ({ onVote }) => {
-  const [users, setUsers] = useState([]);
-  const [selectedOffice, setSelectedOffice] = useState(''); // Added state for selected office
+  const [selectedOffice, setSelectedOffice] = useState('');
+  const [officeCandidates, setOfficeCandidates] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users');
-        const result = await response.json();
-        setUsers(result);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+    if (selectedOffice) {
+      // Group candidates by office
+      const candidatesByOffice = candidates.reduce((acc, candidate) => {
+        const office = candidate.office.toLowerCase();
+        acc[office] = acc[office] || [];
+        acc[office].push(candidate);
+        return acc;
+      }, {});
 
-    fetchData();
-  }, []);
+      // Select candidates for the chosen office
+      const selectedCandidates = candidatesByOffice[selectedOffice.toLowerCase()] || [];
+      setOfficeCandidates(selectedCandidates);
+    }
+  }, [selectedOffice]);
 
   const handleVote = userId => {
-    // Pass both userId and selectedOffice to the onVote function
     onVote(userId, selectedOffice);
   };
 
@@ -39,33 +41,30 @@ const UserList = ({ onVote }) => {
         <option value="Treasurer">Treasurer</option>
         <option value="Secretary">Secretary</option>
       </select>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map(user => (
-            <tr key={user.id}>
-              <td>{user.id}</td>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-              <td>
-                <button
-                  onClick={() => handleVote(user.id)}
-                  disabled={!selectedOffice}
-                >
-                  Vote
-                </button>
-              </td>
+      {selectedOffice && officeCandidates.length > 0 && (
+        <table className="user-list__table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Actions</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {officeCandidates.map(candidate => (
+              <tr key={candidate.id}>
+                <td>{candidate.id}</td>
+                <td>{candidate.name}</td>
+                <td>{candidate.email}</td>
+                <td>
+                  <button onClick={() => handleVote(candidate.id)}>Vote</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
